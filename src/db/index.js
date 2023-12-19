78bc7445413300e753485e756ae48f4a48fcdf6d
vijaykanwal90 <vijaykanwal90@gmail.com>
2023-12-19 10:34:02 +0530

    mongodb connectec

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+const connectDB = async ()=>{
+    try {
+       const connectionIstance =  await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+       console.log(`\n MongoDB connected !!  DB HOST ${connectionIstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection error ",error)
+        process.exit(1);
+    }
+}
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js"
+dotenv.config({
+    path: './env'
+})
+// always try to handle database connectio using try and catch and use await and async beacuase is takes time
+
+/*import express from "express";
+const app = express();
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",()=>{
+            console.log("ERRR",error)
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+    }
+    catch (error) {
+        console.error("Error: ", error)
+        throw err
+    }
+})()
+// here connection is established in the index file it makes the index.js file complex to do it professionalyy we create database connection file in db and then import here
+*/
+connectDB() 
