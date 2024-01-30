b0f6d90c82398e5255dcce62eb81808b006eace5
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-01-30 23:31:50 +0530

    all done

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,3 +3,23 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { ApiError } from "../utils/ApiError.js"
 
+const toggleCommentLike = asyncHandler (async (req,res)=>{
+    const {commentId} = req.params
+
+    const likeComment= await Like.create({
+
+        $set:{
+            
+        }
+    })
+})
+
+export {
+    toggleCommentLike
+}
+// todo
+// toggleCommentLike,
+//     toggleTweetLike,
+//     toggleVideoLike,
+//     getLikedVideos
+
---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,4 @@
+// todo
+// toggleSubscription,
+//     getUserChannelSubscribers,
+//     getSubscribedChannels
